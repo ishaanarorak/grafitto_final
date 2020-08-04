e17995346dc143eaf4c39905997faaecb9b95997
Dhiraj Ghosal <ghosaldhiraj@gmail.com>
2020-08-04 14:03:19 +0530

    corrections

---
+++ js/script.js
@@ -0,0 +1,136 @@
+
+// Open youtube Button
+
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
+// MODAL BOXES
+
+$(document).ready(function() {
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+});
+
+
+// Client List Carousel
+
+$(document).ready(function(){
+   $('.owl-carousel').owlCarousel({
+   loop: true,
+   items: 1,
+   dots: false,
+   autoplay: true,
+   center: true
+});
+});
+
+var x = document.getElementById("logo").autoplay;
+console.log(x)
+
+
+$(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+  });
+
+$(document).ready(function() {
+	$('.popup-gallery1').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
+
+$(document).ready(function() {
+	$('.popup-gallery2').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
+
+$(document).ready(function() {
+	$('.popup-gallery3').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
+
+const slides=document.querySelector(".slider").children;
+const indicatorImages=document.querySelector(".slider-indicator").children;
+
+ for(let i=0; i<indicatorImages.length; i++){
+	 indicatorImages[i].addEventListener("click",function(){
+	  
+		for(let j=0; j<indicatorImages.length; j++){
+		  indicatorImages[j].classList.remove("active");
+		}
+		 this.classList.add("active");
+		 const id=this.getAttribute("data-id");
+		for(let j=0; j<slides.length; j++){
+			slides[j].classList.remove("active");
+		}
+
+		 slides[id].classList.add("active");
+
+	 })
+ }
+
