95d2f8734849c4f0a36bd490d407026801d28a12
ishaan arora <ishaanarorak@gmail.com>
2020-08-08 03:48:13 +0530

    Loader is working and made changes in buttons

--- js/script.js
+++ js/script.js
@@ -80,26 +80,28 @@ $(document).ready(function() {
 });
 
 // Loader JS
+
+
 // document.onreadystatechange = function() { 
 //     if (document.readyState !== "complete") { 
 //         document.querySelector("body").style.visibility = "hidden"; 
-//         document.querySelector("#preloader").style.visibility = "visible"; 
+// 		document.querySelector("#preloader").style.visibility = "visible";
+		
 //     } else { 
 //         document.querySelector("#preloader").style.display = "none"; 
-//         document.querySelector("body").style.visibility = "visible"; 
+// 		document.querySelector("body").style.visibility = "visible"; 
+
 //     } 
 // };
 
-// $(window).on('load', function() { // makes sure the whole site is loaded 
-// 	$('#status').fadeOut(
-		
-// 	); // will first fade out the loading animation 
-// 	console.log("hit");
-// 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-// 	console.log("hit");
-// 	$('body').delay(350).css({'overflow':'visible'});
-// 	console.log("hit");  
-// })
+$(window).on('load', function() { // makes sure the whole site is loaded 
+	$('#status').fadeOut(); // will first fade out the loading animation 
+	console.log("hit");
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	console.log("hit");
+	$('body').delay(350).css({'overflow':'visible'});
+	console.log("hit");  
+})
 
 // WOW JS
 
